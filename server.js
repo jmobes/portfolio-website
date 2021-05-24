bb765f3157e4a84e9c049a885fdba6824b9a5e10
Joseph Moberly <jmobes15@gmail.com>
2021-05-23 21:21:34 -0700

    Implemented email sending with nodemailer. User is able to send an email to my account

--- server.js
+++ server.js
@@ -1,7 +1,9 @@
+require("dotenv").config();
+const nodemailer = require("nodemailer");
 const express = require("express");
 const app = express();
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 5001;
 
 app.use(express.json());
 app.use((req, res, next) => {
@@ -30,7 +32,35 @@ app.post("/email", (req, res, next) => {
     error.code = 400;
     return next(error);
   }
-  res.send({ name, email, message });
+
+  let transporter = nodemailer.createTransport({
+    service: "gmail",
+    auth: {
+      user: process.env.TRANSPORTER_EMAIL,
+      pass: process.env.TRANSPORTER_PASS,
+    },
+  });
+
+  let mailOptions = {
+    from: email,
+    to: process.env.MY_EMAIL,
+    subject: "Inquiry from Portfolio Site",
+    text: `
+    FROM: ${name}
+    EMAIL: ${email}
+    MESSAGE: ${message}
+      `,
+  };
+
+  transporter.sendMail(mailOptions, (err, data) => {
+    if (err) {
+      const error = new Error("Unable to send email. Please try again.");
+      error.code = 500;
+      return next(error);
+    } else {
+      res.send("Email sent");
+    }
+  });
 });
 
 app.use((error, req, res, next) => {
